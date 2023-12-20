3326558b3d0dff73aeb19bf9841dc746974bf3c8
Saumya Saraswat <52417545+MagmaCubes1133@users.noreply.github.com>
2023-12-20 12:51:17 +0530

    Fix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -12,7 +12,7 @@ module.exports = {
 		// Test the message content against the regex
 		if (regex.test(message.content)) {
 			// Send a response tagging the author and including the message text
-			message.channel.send(`${message.author}, Has paid their respects. F.`);
+			message.channel.send(`${message.author} has paid their respects. F.`);
 		}
 	},
 };
